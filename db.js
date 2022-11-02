7845bb614e819f630d46037832d820bd768e24c0
fawazorg <foazorg@gmail.com>
2022-11-02 13:30:45 +0300

    change docker port

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@127.0.0.1/${process.env.MONGO_DB_NAME}`,
+  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@127.0.0.1:27019/${process.env.MONGO_DB_NAME}`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
